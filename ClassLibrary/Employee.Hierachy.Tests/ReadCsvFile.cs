14e2c58ca464def570b1ca55ad6fa6ce66ddf995
devSecOps <36701027+d3vSecOps@users.noreply.github.com>
2020-12-07 09:14:43 +0300

    Add files via upload

---
+++ ClassLibrary/Employee.Hierachy.Tests/ReadCsvFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Employees.Hierachy.Tests
+{
+    public class ReadCsvFileLine
+    {
+        [Fact]
+        public void ReadCsvFileLines_Returns_NewEmployee()
+        {
+            string line = "2,1,1000000";
+            var employee = ReadCsvFileLines.EmployeeFromCsvLine(line);
+            Assert.Equal("1", employee.ManagerId);
+        }
+        [Fact]
+        public void ReadCsvFileLines_ReturnsNull_WhenInputIsInvalid()
+        {
+            string line = "1,0100";
+            var employee = ReadCsvFileLines.EmployeeFromCsvLine(line);
+            Assert.Null(employee);
+        }
+    }
+}
